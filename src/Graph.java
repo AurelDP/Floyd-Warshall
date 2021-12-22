5ae26af66fdbd21a4d539fbb948794496c0cb8bf
SCPWernert <73222657+SCPWernert@users.noreply.github.com>
2021-12-22 17:40:06 +0100

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,28 +1,20 @@
-import java.util.ArrayList;
+package projet_floyd;
 
 public class Graph {
-	private int nbrStates;
-	private int nbrTransitions;
-	private ArrayList<ArrayList<Integer>> adjacencyMatrix;
 	
-	public Graph(int nbrStates, int nbrTransitions, ArrayList<ArrayList<Integer>> adjacencyMatrix) {
+	private int nbrStates;
+	private int[][] adjacencyMatrix;
+	
+	public Graph(int nbrStates, int[][] adjacencyMatrix) {
 		this.nbrStates = nbrStates;
-		this.nbrTransitions = nbrTransitions;
-		this.adjacencyMatrix = adjacencyMatrix;
+		this.adjacencyMatrix = new int[nbrStates][nbrStates];
+		this.adjacencyMatrix = (int[][]) adjacencyMatrix.clone();
 	}
 	
 	public Graph(Graph graph) {
 		this.nbrStates = graph.nbrStates;
-		this.nbrTransitions = graph.nbrTransitions;
-		this.adjacencyMatrix = new ArrayList<ArrayList<Integer>>();
-		int u = 0;
-		for (ArrayList<Integer> aI : graph.adjacencyMatrix) {
-			this.adjacencyMatrix.add(new ArrayList<Integer>());
-			for (Integer i : aI) {
-				this.adjacencyMatrix.get(u).add(i);
-			}
-			u ++;
-		}
+		this.adjacencyMatrix = new int[this.nbrStates][this.nbrStates];
+		this.adjacencyMatrix = (int[][]) graph.adjacencyMatrix.clone();;
 	}
 	
 	/*-----------------------------------------------------------------------------
@@ -37,62 +29,19 @@ public class Graph {
 		this.nbrStates = nbrStates;
 	}
 
-	public int getNbrTransitions() {
-		return nbrTransitions;
-	}
-
-	public void setNbrTransitions(int nbrTransitions) {
-		this.nbrTransitions = nbrTransitions;
-	}
-
-	public ArrayList<ArrayList<Integer>> getAdjacencyMatrix() {
+	public int[][] getAdjacencyMatrix() {
 		return adjacencyMatrix;
 	}
 
-	public void setAdjacencyMatrix(ArrayList<ArrayList<Integer>> adjacencyMatrix) {
-		this.adjacencyMatrix = adjacencyMatrix;
+	public void setAdjacencyMatrix(int[][] AdjacencyMatrix) {
+		this.adjacencyMatrix = (int[][]) AdjacencyMatrix.clone();
 	}
 	
-	public static void printMatrix(int dist[][]) {
-		System.out.println("New Matrix: ");
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				if (dist[i][j] == I)
-					System.out.print("I   \t");
-				else
-					System.out.print(dist[i][j] + "   \t");
-			}
-			System.out.println();
-		}
-	}
-
-	private static Booleen isAbsorbent(int dist[][]) {
-		for (int k = 0, k < dist.lenght, k++) {
-			if (dist[k][k] < 0)
-				return true;
-		}
-		return false;
-	}
+	/*-----------------------------------------------------------------------------
+	 * methods
+	 ----------------------------------------------------------------------------*/
 	
-	private static void FloydWarshall(int tab[][]) {
-		int n = tab.lenght;
-		int[][] dist = new int[n][n];
-		int i, j, k;
 	
-		dist = tab;
 	
-		// Floyd-Warshall Algorithm
-		for (k = 0; k < n; k++) {
-			for (j = 0; j < n; j++) {
-				for (i = 0; i < n; i++) {
-					// If k is on the shortest path from
-					// i to j, then update the value of dist[i][j]
-					if (dist[i][k] + dist[k][j] < dist[i][j])
-						dist[i][j] = dist[i][k] + dist[k][j];
-				}	
-			}	
-		}
-		printMatrix(dist);
-	}
 	
 }
